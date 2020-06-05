1c474e6f6fa3ac27f9511de447eb4ac72127d260
MCauz <michaelhcausey@gmail.com>
2020-06-05 13:03:10 -0400

    class methods started

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,32 @@
+require './cat.rb'
+require './dog.rb'
+
 class Owner
   # code goes here
+  attr_reader :name, :species
+  attr_writer
+  attr_accessor
+  @@all = [] 
+
+  def initialize (name, species="human")
+    @name = name
+    @species = species
+    say_species
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def Owner.all
+    return @@all
+  end
+
+  def Owner.count
+    return @@all.count
+  end
+
+
+
 end
