0e96b950e9136b9f524724cdee8c83a6240b34bc
BartTheScrivner <dframe22@gmail.com>
2020-06-05 16:20:28 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -24,7 +24,7 @@ class Owner
   end
 
   def self.reset_all
-    self.all.clear #Accessing class varible directly?
+    self.all.clear 
     
   end
 
@@ -37,8 +37,8 @@ class Owner
   
   def dogs
 
-    Dog.all.select do |dok|
-      dok.owner == self
+    Dog.all.select do |dog|
+      dog.owner == self
     end
   end
 
@@ -52,19 +52,16 @@ class Owner
   end
 
    def walk_dogs
-    Dog.all.each {|dog| dog.mood = 'happy'} 
+    self.dogs.each {|dog| dog.mood = 'happy'} 
   end
 
   def feed_cats
-    Cat.all.each {|cat| cat.mood = 'happy'}
+    self.cats.each {|cat| cat.mood = 'happy'}
   end
 
   def sell_pets
-    Dog.all.each do |dog| 
-      dog.mood = 'nervous' 
-      dog.owner = nil
-    end
-    Cat.all.each {|cat| cat.mood = 'nervous'; cat.owner = nil}
+    self.dogs.each {|dog| dog.mood = 'nervous'; dog.owner = nil}
+    self.cats.each {|cat| cat.mood = 'nervous'; cat.owner = nil}
   end
 
   def list_pets
