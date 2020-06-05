168c3477565bde31b4517be4f0ce6ecdfa067b0f
Issa sangare <issabmsangare@hotmail.com>
2020-06-05 14:46:34 -0400

    solved buy_dog meththod

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Owner
   attr_reader :name, :species
 
@@ -23,8 +25,31 @@ class Owner
 
   def self.reset_all
     self.all.clear #Accessing class varible directly?
+    
   end
 
+  def cats
+
+    Cat.all.select do |kat|
+      kat.owner == self
+    end
+  end
   
+  def dogs
+
+    Dog.all.select do |dok|
+      dok.owner == self
+    end
+  end
+
+  def buy_cat(new_cat)
+   Cat.new(new_cat, self)
+
+  end
+
+  def buy_dog(new_dog)
+    Dog.new(new_dog, self)
+  end
+
 
 end
